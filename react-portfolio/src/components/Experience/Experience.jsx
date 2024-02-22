11195c3617f97dc645d85f36813ae14b111c3580
rhit-kohji <koh.james99@gmail.com>
2024-02-21 23:17:57 -0500

    added images

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import skills from "../../data/skills.json"
+import history from "../../data/history.json"
+import { getImageUrl } from '../../utils'
 
 export const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id="experience">
+        <h2>Experience</h2>
+        <div>
+            <div>
+              {skills.map((skill, id) => {
+                return (<div key={id}>
+                  <div>
+                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title}/>
+                  </div>
+                  <p>{skill.title}</p>
+                </div>)
+              })}
+            </div>
+            <ul>
+              {
+                history.map((historyItem, id) => {
+                    return (
+                      <li key={id}>
+                        <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`}></img>
+                      </li>
+                    )
+                })
+              }
+            </ul>
+        </div>
+    </section>
   )
 }
