fac734878da29f507c887e657da3991ee14375b6
rhit-kohji <koh.james99@gmail.com>
2024-06-18 15:07:21 -0400

    changed repo name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kohji.github.io",
+  base: "/rhit-kohji.github.io",
   plugins: [react()],
   css: {
     modules: {
